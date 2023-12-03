65b64fe1586f8c6afc7b3839a89440f79cba841b
Dexter シ <61196803+jaxonfromkh@users.noreply.github.com>
2023-12-04 01:40:09 +0700

    Initial commit

---
+++ web/src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { CONFIG, IS_BROWSER } from './stores/stores';
+    import AlwaysListened, { InitialiseListen } from '@utils/listeners';
+    import Visibility from '@providers/Visibility.svelte';
+    import Debug from '@providers/Debug.svelte';
+    import ImageHolder from '@components/ImageHolder.svelte';
+
+    CONFIG.set({
+        fallbackResourceName: 'debug',
+        allowEscapeKey: true,
+    });
+
+    InitialiseListen();
+</script>
+
+<Visibility>
+    <ImageHolder />
+</Visibility>
+
+{#if $IS_BROWSER}
+    <Debug />
+{/if}
